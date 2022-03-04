cdf22caa4f0e9daa7e15235ddeb1ca5427282e9c
Sachinhugar <98160656+Sachinhugar@users.noreply.github.com>
2022-03-04 11:41:13 +0530

    Add files via upload

---
+++ Grid/registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * I95dev_Grid Module Registration
+ *
+ * @category    I95dev
+ * @package     I95dev_Grid
+ * @author      I95dev Software Private Limited
+ *
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'I95dev_Grid',
+    __DIR__
+);
+?>
